89bc7caa1591f3eb83f76999d08603e6360333cc
Beldaras <jphilippe.roblot@gmail.com>
2023-02-02 10:27:54 +0100

    DB tables activities and populars created

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,14 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
+CREATE TABLE activities (
+  `id` int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `image` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` TEXT NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+
+CREATE TABLE populars (
+  `id` int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `picture` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` TEXT NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
