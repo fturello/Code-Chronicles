764a2caac180d2dc306b73c81c696b687572f10c
Baptcave <[noye.baptiste@orange.fr](mailto:noye.baptiste@orange.fr)>
2023-02-02 14:51:33 +0100

    work in progress on activities page

--- frontend/src/components/Activities.jsx
+++ frontend/src/components/Activities.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import PopUp from './PopUp';
 
@@ -8,10 +8,18 @@ import styles from '../styles/Activities.module.css';
 function Activities() {
   const [activePopUp, setActivePopUp] = useState(true);
 
+  // useEffect(() => {
+  //   axios.get('http://localhost:8000/scenario_cards')
+  //     .then((res) => res.data)
+  // }, []);
+
   return (
     <div className={styles.general}>
       {activePopUp && <PopUp setActivePopUp={setActivePopUp}/>}
       <div class={styles.card1}>
+        <p><span>HOVER ME</span></p>
+        <p><span>HOVER ME</span></p>
+        <p><span>HOVER ME</span></p>
         <p><span>HOVER ME</span></p>
         <p><span>HOVER ME</span></p>
         <p><span>HOVER ME</span></p>
