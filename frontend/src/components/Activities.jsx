f5adf1c03560626fe7d2bb790aef4f0e02519112
Baptcave <[noye.baptiste@orange.fr](mailto:noye.baptiste@orange.fr)>
2023-02-02 16:47:24 +0100

    saving changes before pulling dev

--- frontend/src/components/Activities.jsx
+++ frontend/src/components/Activities.jsx
@@ -17,19 +17,26 @@ function Activities() {
     <div className={styles.general}>
       {activePopUp && <PopUp setActivePopUp={setActivePopUp} />}
       <div className={styles["cards-container"]}>
-        <div class={styles.card1}>
-          <div><p><span>HOVER ME</span></p></div>
-          <div><p><span>HOVER ME</span></p></div>
-          <div><p><span>HOVER ME</span></p></div>
-          <div><p><span>HOVER ME</span></p></div>
-          <div><p><span>HOVER ME</span></p></div>
+        <div class={styles.cards}>
+          <div>
+            <p>
+              <span className={styles.span}>
+                HOVER ME
+              </span>
+            </p>
+            <img className={styles["activity-image"]} src='src/assets/Bal1.png' alt='' />
+          </div>
+          <div><p><span className={styles.span}>HOVER ME</span></p></div>
+          <div><p><span className={styles.span}>HOVER ME</span></p></div>
+          <div><p><span className={styles.span}>HOVER ME</span></p></div>
+          <div><p><span className={styles.span}>HOVER ME</span></p></div>
         </div>
-        <div class={styles.card1}>
-          <div><p><span>HOVER ME</span></p></div>
-          <div><p><span>HOVER ME</span></p></div>
-          <div><p><span>HOVER ME</span></p></div>
-          <div><p><span>HOVER ME</span></p></div>
-          <div><p><span>HOVER ME</span></p></div>
+        <div class={styles.cards}>
+          <div><p><span className={styles.span}>HOVER ME</span></p></div>
+          <div><p><span className={styles.span}>HOVER ME</span></p></div>
+          <div><p><span className={styles.span}>HOVER ME</span></p></div>
+          <div><p><span className={styles.span}>HOVER ME</span></p></div>
+          <div><p><span className={styles.span}>HOVER ME</span></p></div>
         </div>
       </div>
     </div>
